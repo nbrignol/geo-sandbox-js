f9a0f0be74e948899bcebcf7c321e7089f1c254e
Nicolas Brignol <tools@octet-studio.com>
2015-05-05 15:58:56 +0200

    Show current position

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 Geo Sandbox : maps, GPS and compass. 
 Javascript powered.
+
+Some WMTS servers :
+
+* http://tile.openstreetmap.org/{z}/{x}/{y}.png
+* http://tile.stamen.com/toner/{z}/{x}/{y}.png
+* http://tile.stamen.com/watercolor/{z}/{x}/{y}.jpg
+
--- js/main.js
+++ js/main.js
@@ -1,42 +1,35 @@
 document.addEventListener('DOMContentLoaded', function(){
 	
-	//map (leaflet : http://leafletjs.com/examples/quick-start.html)
+	var status = document.querySelector("#status");
+
+	//map 
 	var map = L.map('myMap');
 	map.setView([0, 0], 3);
 
-	//map pattern (WTMS). mapbox, ign, osm, ... 
-	//var mapPatternUrl = "http://tile.stamen.com/toner/{z}/{x}/{y}.png";
-    //var mapPatternUrl = "http://tile.stamen.com/watercolor/{z}/{x}/{y}.jpg";
-	
+	//pattern
 	var mapPatternUrl = 'http://tile.openstreetmap.org/{z}/{x}/{y}.png';
 	var tileLayer = L.tileLayer(mapPatternUrl);
 	tileLayer.addTo(map);
 
 	//marker
 	var marker = L.marker([0, 0]);
-	marker.bindPopup("The marker.");
+	marker.bindPopup("Me.");
 	marker.addTo(map);
 
-	//shape circle
-	var circle = L.circle([0, 0], 500000);
-	circle.bindPopup("The circle.");
-	circle.addTo(map);
+	function processPosition(event){
+		status.innerHTML = "Lat : " + event.coords.latitude + "° Long : " + event.coords.longitude + "° Precision : " + event.coords.accuracy + "m.";
+		marker.setLatLng( [event.coords.latitude, event.coords.longitude] );
 
-	//shape polygon
-	var polygon = L.polygon([
-	    [-1, -1],
-	    [1, -1],
-	    [1, 1],
-	    [-1, 1],
-	]);
-	polygon.bindPopup("The polygon.");
-	polygon.addTo(map);
-
-	/*
-	function onMapClick(e) {
-		marker.setLatLng(e.latlng);
 	}
-	map.on('click', onMapClick);
-	*/
+
+	function errorPosition(){
+		status.innerHTML = "No position.";
+		marker.setLatLng( [0,0] );
+	}
+
+	//location notifications
+	var options = {"enableHighAccuracy": true, "maximumAge" : 0, "timeout" : Infinity};
+	navigator.geolocation.watchPosition( processPosition, errorPosition, options );
+
 
 });
